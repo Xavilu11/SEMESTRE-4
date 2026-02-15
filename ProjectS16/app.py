5328007f4a35600e80d71814daea0d2bbbd0242c
Xavilu11 <gj.lunat@uea.edu.ec>
2026-02-15 17:54:47 -0500

    ProjectS16

--- ProjectS16/app.py
+++ ProjectS16/app.py
@@ -28,4 +28,6 @@ def contacto():
     return "Contáctanos en Harta Pinta – Estamos aquí para ayudarte."
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
